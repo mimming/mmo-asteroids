66897a9407217e5fb8c9e2d26f46f5fc67bfb049
Jenny Tong <jen@firebase.com>
2015-06-10 14:24:51 -0700

    startup script now works

--- bot/gce-startup-script.sh
+++ bot/gce-startup-script.sh
@@ -2,8 +2,15 @@
 # This is a startup script for Google Compute Engine
 # Learn more about startup scripts: https://cloud.google.com/compute/docs/startupscript
 
+# environment
+export PATH=$PATH:/opt/bitnami/nodejs/bin:/opt/bitnami/git/bin
+
+# install dependencies
+npm install -g forever
+
+# start the bot
 pushd /usr/local
 git clone https://github.com/mimming/mmo-asteroids
-pushd /usr/local/mmo-asteroids/bot
-/opt/bitnami/nodejs/bin/node ./bot.js&
-
+pushd mmo-asteroids/bot
+npm install
+forever start /usr/local/mmo-asteroids/bot/bot.js
